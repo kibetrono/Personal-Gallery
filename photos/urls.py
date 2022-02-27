4e96c22e7eb098c3f68e7167f261221a2f5c6e7d
email <kibetdavidro@gmail.com>
2022-02-27 19:10:57 +0300

    update the urlpatterns to fit the add photo button

--- photos/urls.py
+++ photos/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path,re_path
 from . import views
 
+
 urlpatterns=[
     path('',views.home,name='home'),
     path('search/', views.search, name='search'),
     path('location/<int:image_location>/', views.location, name='location'),
     path('category/<str:pk>/', views.category, name='category'),
+    path('new_image/', views.registerPhoto, name='new_image'),
+
 ]
