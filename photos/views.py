b16b4390887d2f93d09bc6303efa5b9a1cd40aed
email <kibetdavidro@gmail.com>
2022-02-27 11:08:22 +0300

    add search view

--- photos/views.py
+++ photos/views.py
@@ -14,3 +14,22 @@ def home(request):
     context = {'title': title, 'images': image[:8], 'locations': location, 'categories': category}
     return render(request, 'photos/home.html', context)
 
+
+def search(request):
+    location = Location.objects.all()  # For purpose of navbar
+
+    title = "Search"
+    if 'search_query' in request.GET and request.GET["search_query"]:
+        search_term = request.GET.get("search_query").lower()
+        searched_results = Image.filter_by_category(search_term)
+
+        message = f"{search_term}"
+        context = {'message': message, 'results': searched_results, 'title': title, 'locations': location}
+
+        return render(request, 'photos/search.html', context)
+
+    else:
+        messages.error(request, "You haven't searched for any term")
+        message = "You haven't searched for any term"
+        return render(request, 'photos/search.html', {"message": message})
+
