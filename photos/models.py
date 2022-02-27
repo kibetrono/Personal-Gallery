924510f1164056bfb150b41f13197bee8119f932
email <kibetdavidro@gmail.com>
2022-02-27 08:52:51 +0300

    add category  class model

--- photos/models.py
+++ photos/models.py
@@ -29,3 +29,31 @@ class Location(models.Model):
     def __str__(self):
         return self.location_name
 
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-category_name']
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self, name):
+        self.name = name
+        self.save()
+
+    def __str__(self):
+        return self.category_name
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        photo = cls.objects.filter(category_name__icontains=search_term)
+        return photo
+
+
